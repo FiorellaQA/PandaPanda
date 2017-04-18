a0740b4c8e61d7cc0fd0687c3dfde0fceec2878e
FiorellaQA <fiorella_90_17@hotmail.com>
2017-04-18 00:49:53 -0500

    implementando funcionalidad js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 document.getElementById("bt-Origen").addEventListener("click",function(e){
-	e.preventDefault();
-	document.getElementById("contentHistoria").style.display = "none";
+	document.getElementById("contentOrigen").classList.toggle("hidden");
+});
+document.getElementById("bt-Historia").addEventListener("click",function(e){
+	document.getElementById("contentHistoria").classList.toggle("hidden");
+});
 
+var array = document.getElementsByClassName("close");
+for( var i = 0 ; i < array.length ; i++){
+	array[i].addEventListener("click",eliminar);
+}
+ function eliminar(e){
+ 	e.target.parentNode.style.display = "none";	
+ }
+document.getElementById("bt-Rest").addEventListener("click",function(e){
+	var arr = document.getElementsByClassName("content-img");
+	for( var i = 0 ; i < arr.length ; i++){
+		arr[i].style.display = "block";
+	}
 });
